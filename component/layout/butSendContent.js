a743b64c0b68ccb0502c0204ef74d9ebca4707aa
jarangit <jaran.dch@gmail.com>
2020-05-05 12:42:43 +0700

    add but select

--- component/layout/butSendContent.js
+++ component/layout/butSendContent.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import Collaborate from '../../lib/img/Microsite/Icon/Collaborate.png'
+import { withRouter } from 'next/router'
 
 const ModalSty = styled.div`
     position: fixed;
@@ -30,14 +31,30 @@ const ModalSty = styled.div`
 `
 
 const ButSendCon = (props) => {
+    console.log(props.pathName)
+    const CheckPathName = () => {
+        if (props.pathName === undefined){
+            return(
+                <div onClick = {props.func}>
+                    <img  src = {Collaborate} width = '40'/>
+                    <h3 >ร่วมส่งกำลังใจให้ หมอ - พยาบาล</h3>
+                </div>
+            )
+        } else if (props.pathName === '/select'){
+            return(
+                <div onClick = {props.func}>
+                    <h3 >เลือก</h3>
+                </div>
+            )
+        }else {
+            return''
+        }
+    }
     return(
         <ModalSty>
-            <div onClick = {props.func}>
-                <img  src = {Collaborate} width = '40'/>
-                <h3 >ร่วมส่งกำลังใจให้ หมอ - พยาบาล</h3>
-            </div>
+            {CheckPathName()}
         </ModalSty>
     )
 }
 
-export default ButSendCon
+export default withRouter(ButSendCon)
--- pages/select.js
+++ pages/select.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Layout from '../component/layout/layout'
+import ButSendCon from '../component/layout/butSendContent'
+import { withRouter } from 'next/router'
 
-const SelectPage = () => {
+const SelectPage = ({router}) => {
     return(
         <Layout>
             this select page
+            <ButSendCon pathName = {router.pathname}/>
         </Layout>
     )
 }
 
-export default SelectPage
+export default withRouter(SelectPage)
