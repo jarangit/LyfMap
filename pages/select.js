5d6bf4eca1f06ae26beb59a32cf2a48acd19159f
jarangit <jaran.dch@gmail.com>
2020-05-05 13:59:56 +0700

    styled select page

--- pages/select.js
+++ pages/select.js
@@ -2,11 +2,35 @@ import React from 'react'
 import Layout from '../component/layout/layout'
 import ButSendCon from '../component/layout/butSendContent'
 import { withRouter } from 'next/router'
+import styled from 'styled-components'
+import BgImg  from '../lib/img/Microsite/Line.jpg'
+import HeartImg from '../lib/img/Microsite/Icon/Heart.png'
+
+const SelectPageSty = styled.div`
+    div{
+        display:flex;
+        justify-content:center;
+        margin: 20px 0;
+    }
+    /* background-image: url(${BgImg}); */
+    /* background-repeat: no-repeat; */
+    height: 2000px;
+`
 
 const SelectPage = ({router}) => {
+    console.log(BgImg)
     return(
         <Layout>
-            this select page
+            <SelectPageSty>
+                <div>
+                    <h2>เลือกส่งต่อกำลังใจ</h2>
+                    <img src = {HeartImg} width = '40'/>
+                </div>
+                <div>
+                    this slide tap
+                </div>
+                    <img  src = {BgImg} />
+            </SelectPageSty>
             <ButSendCon pathName = {router.pathname}/>
         </Layout>
     )
